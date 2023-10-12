54eb0d09c2c241fda12b37d034065e5704a880fb
rueAlter67 <emma_cacatian@dlsu.edu.ph>
2023-10-12 23:23:01 +0800

    Added behaviors for Area

--- src/Area.java
+++ src/Area.java
@@ -1,3 +1,53 @@
 public class Area {
+
+    // fields
+    private int nAreaLevel;
+    private int nXDim; 
+    private int nYDim;
+    private Creature CEnemy; 
     
+    public Area(int nAreaLevel, int nXDim, int nYDim, Creature CEnemy)
+    {
+        this.nAreaLevel = nAreaLevel; 
+        
+        if(nAreaLevel == 1)
+        {
+            this.nXDim = 5; 
+            this.nYDim = 1;
+        }
+        else if(nAreaLevel == 2)
+        {
+            this.nXDim = 3; 
+            this.nYDim = 3;
+        }
+        else
+        {
+            this.nXDim = 4; 
+            this.nYDim = 4;
+        }
+
+        this.CEnemy = null; 
+
+    }
+
+
+    public void loadArea(Player CPlayer)
+    {
+        int nCol; 
+        int nRow; 
+
+        for(nCol = 0; nCol < this.nYDim; nCol++)
+        {
+            for(nRow = 0; nRow < this.nXDim; nRow++)
+            {
+                if(nRow ==  CPlayer.getPosX() && nCol == CPlayer.getPosY())
+                    System.out.print("O ");
+                else 
+                    System.out.print("+ ");
+            }
+
+            System.out.print("\n");
+        }
+    }
+
 }
--- src/tryOUTS/display.java
+++ src/tryOUTS/display.java
@@ -3,5 +3,25 @@ import java.util.Scanner;
 public class display 
 {
     public static void main(String[] args) {
+
+        int row, col; 
+        int x = 5; 
+        int y = 1; 
+        int playerX = 0;
+        int playerY = 0; 
+        System.out.println("\n");
+        
+        for(col = 0; col < y; col++)
+        {
+            for(row = 0; row < x; row++)
+            {
+                if(row == playerX && col == playerY)
+                    System.out.print("O ");
+                else 
+                    System.out.print("+ ");
+            }
+
+            System.out.print("\n");
+        }
     }
 }
